3437a949099a34ec891dec2b6e8e86d846d3fd32
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-03-02 17:04:59 +0100

    use context api following classes examples (to be reviewed)

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProviderWrapper } from "./context/auth.context";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProviderWrapper>
+        <App />
+      </AuthProviderWrapper>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
