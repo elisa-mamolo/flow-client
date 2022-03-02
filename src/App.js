e06dcbebd8ea7884d5b13bac28ebad1e50236f9d
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-03-02 17:04:14 +0100

    add routes to app.js and tested signup and login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
