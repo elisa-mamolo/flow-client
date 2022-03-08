eff75564e4eade1fc2859433f25a27267f46a04b
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-03-08 09:07:10 +0100

    started styling acquarium page

--- src/pages/AcquariumPage.js
+++ src/pages/AcquariumPage.js
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import AddAcquarium from "../components/AddAcquarium";
 import LogRow from "../components/LogRow";
-import { Button, Card, Table } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Container,
+  Navbar,
+  Table,
+  Row,
+  Col,
+} from "react-bootstrap";
 import NavBar from "../components/NavBar";
+import { AuthContext } from "../context/auth.context";
 
 const API_URL = "http://localhost:5005";
 
@@ -44,75 +53,87 @@ function AcquariumPage(props) {
 
   return (
     <div>
-      {/* <NavBar /> */}
+      <NavBar />
       <h1>Your Acquariums</h1>
-      {/* <AddAcquarium refreshAcquariums={getAcquariums} /> */}
 
       {acquariums && (
         <div>
-          {acquariums.map((item) => (
-            <div key={item._id}>
-              <Card style={{ width: "18rem" }} onClick={setShowLogHandler}>
-                <Card.Img
-                  variant="top"
-                  src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBwgIDQcIBwgIDQ0ICAcHBw8IDQgNFREWFhURFRMYHSggGBolGxMTITEhJSkrPi4uFx8zODMtQygtNSsBCgoKBgYFDg8PDisZExkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIALgBEwMBIgACEQEDEQH/xAAWAAEBAQAAAAAAAAAAAAAAAAAAAQf/xAAdEAEAAQQDAQAAAAAAAAAAAAAAEQEhQWFRofGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/AMNAAAAAAAAAAAAAAAAAAWtZ2gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAuN8oAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAuJ6QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABa2zM3sgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALiZ+cggAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALSk+wCAAAAAAAAAAAAAAAAAAqAAAAAAAAptAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/9k="
-                />
-                <Card.Body>
-                  <Card.Title>{item.name}</Card.Title>
-                  <Card.Text></Card.Text>
-                  <Button onClick={() => deleteAcquarium(item._id)}>
-                    Delete
-                  </Button>
-                  <Link to={`/edit-acquarium/${item._id}`}>
-                    <Button>Edit</Button>
-                  </Link>
-                </Card.Body>
-              </Card>
-
-              <div>
-                {showlog ? (
-                  <div>
-                    <Table striped bordered hover>
-                      <tbody>
-                        <tr>
-                          <th>Date</th>
-                          <th>alkalinity</th>
-                          <th>ammonia</th>
-                          <th>calcium</th>
-                          <th>magnesium</th>
-                          <th>nitrate</th>
-                          <th>nitrite</th>
-                          <th>ph</th>
-                          <th>phosphate</th>
-                          <th>salinity</th>
-                          <th>temperature</th>
-                        </tr>
-                        {item.logs.map((log) => (
-                          <LogRow
-                            logRow={log.measurements}
-                            logRowAcquarium={log.acquarium}
-                            logRowId={log._id}
-                            key={log._id}
-                          />
-                        ))}
-                      </tbody>
-                    </Table>
-                    <Link to={`/addlog/${item._id}`}>
-                      <Button>Add Log</Button>
-                    </Link>
-                  </div>
-                ) : (
-                  <Link to={`/addlog/${item._id}`}>
-                    <Button>Add Log</Button>
-                  </Link>
-                )}
+          <Container>
+            <Row xs={2} md={4} lg={6}>
+              <Col>
                 <Button onClick={() => setShowAddAcquarium(!showAddAcquarium)}>
                   Add Acquarium
                 </Button>
-                {showAddAcquarium ? <AddAcquarium /> : <p></p>}
-              </div>
-            </div>
-          ))}
+              </Col>
+              <Col>
+                {acquariums.map((item) => (
+                  <div key={item._id}>
+                    <Card
+                      style={{ width: "18rem" }}
+                      onClick={setShowLogHandler}
+                    >
+                      <Card.Img
+                        variant="top"
+                        src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBwgIDQcIBwgIDQ0ICAcHBw8IDQgNFREWFhURFRMYHSggGBolGxMTITEhJSkrPi4uFx8zODMtQygtNSsBCgoKBgYFDg8PDisZExkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIALgBEwMBIgACEQEDEQH/xAAWAAEBAQAAAAAAAAAAAAAAAAAAAQf/xAAdEAEAAQQDAQAAAAAAAAAAAAAAEQEhQWFRofGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/AMNAAAAAAAAAAAAAAAAAAWtZ2gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAuN8oAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAuJ6QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABa2zM3sgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALiZ+cggAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALSk+wCAAAAAAAAAAAAAAAAAAqAAAAAAAAptAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/9k="
+                      />
+                      <Card.Body>
+                        <Card.Title>{item.name}</Card.Title>
+                        <Card.Text>{item.started}</Card.Text>
+                        <Card.Text>{item.liters}</Card.Text>
+
+                        <Link to={`/edit-acquarium/${item._id}`}>
+                          <Button>Edit</Button>
+                        </Link>
+                        <Button onClick={() => deleteAcquarium(item._id)}>
+                          Delete
+                        </Button>
+                        <Link to={`/addlog/${item._id}`}>
+                          <Button>Add Log</Button>
+                        </Link>
+                        <Link to={`/charts/${item._id}`}>
+                          <Button>Charts</Button>
+                        </Link>
+                      </Card.Body>
+                    </Card>
+
+                    {showlog ? (
+                      <div>
+                        <Table striped bordered hover>
+                          <tbody>
+                            <tr>
+                              <th>Date</th>
+                              <th>alkalinity</th>
+                              <th>ammonia</th>
+                              <th>calcium</th>
+                              <th>magnesium</th>
+                              <th>nitrate</th>
+                              <th>nitrite</th>
+                              <th>ph</th>
+                              <th>phosphate</th>
+                              <th>salinity</th>
+                              <th>temperature</th>
+                            </tr>
+                            {item.logs.map((log) => (
+                              <LogRow
+                                logRow={log.measurements}
+                                logRowAcquarium={log.acquarium}
+                                logRowId={log._id}
+                                key={log._id}
+                              />
+                            ))}
+                          </tbody>
+                        </Table>
+                      </div>
+                    ) : (
+                      <p></p>
+                    )}
+
+                    {showAddAcquarium ? <AddAcquarium /> : <p></p>}
+                  </div>
+                ))}
+              </Col>
+            </Row>
+          </Container>
         </div>
       )}
     </div>
